f1107266f4d57868284d4b4a6d15ce670ce6a503
Kefir101 <kefir.sade.bar@gmail.com>
2021-05-20 08:46:27 -0700

    master

--- sketch.js
+++ sketch.js
@@ -51,12 +51,6 @@ function checkInput() {
 }
 
 
-var path = window.location.pathname;
-console.log(path);
-var page = path.split("/").pop();
-console.log(page);
-
-
 
 var cookies = 0;
 var startClicking = true; 
