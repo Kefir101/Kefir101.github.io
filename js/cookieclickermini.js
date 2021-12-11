a30d508232b21c2969c6800e720be1ab9c6302f0
Kefir101 <kefir.sade.bar@gmail.com>
2021-12-10 17:38:16 -0800

    added upgrades, bug fixes

---
+++ js/cookieclickermini.js
@@ -0,0 +1,20 @@
+var cookies = 0;
+var startClicking = false;
+function clicked() {
+  cookies++;
+  if (!startClicking) {
+    startClicking = true; //doesnt affect value in addTime for some reason
+    addTime()
+  }
+  var element = document.getElementById("cookiep2");
+  element.innerHTML = "Cookies: " + cookies;
+}
+var sec = 0;
+function addTime() {
+  if (startClicking) {
+    sec++;
+    var newHTML = "Cookies clicked per second: " + Math.round(cookies / sec);
+    cpstimer.innerHTML = newHTML; //how???
+    setTimeout("addTime()", 1000);
+  }
+}
